4db97047df2267d737d84691c33142ce5a7e4a0b
Sirinart2541 <narmnarm44@gmail.com>
2019-05-24 22:44:39 +0700

    stu,sub,fac,grade

--- main.py
+++ main.py
@@ -23,14 +23,17 @@ def hello_world():
     rows = cursor.fetchall()
     resp = jsonify(rows)
     resp.status_code = 200
+    cursor.close()
+    cnx.close()
     return resp
 
-@app.route('/<id>')
-def stu(id):
+@app.route('/student')
+def student():
     try:
         cnx = mysql.connect()
         cursor = cnx.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM student WHERE ID = %s", id)
+        cursor.execute("SELECT ID,FIRST_NAME,LAST_NAME,GENDER,BDATE,ADDRESS,STATUS,faculty.FACULTY_ID,faculty.FACULTY_NAME \
+                        FROM student,faculty WHERE student.FACULTY_ID=faculty.FACULTY_ID")
         rows = cursor.fetchall()
         resp = jsonify(rows)
         resp.status_code = 200
@@ -40,3 +43,82 @@ def stu(id):
     finally:
         cursor.close()
         cnx.close()
+
+@app.route('/student/<id>')
+def student_id(id):
+    try:
+        cnx = mysql.connect()
+        cursor = cnx.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT ID,FIRST_NAME,LAST_NAME,GENDER,BDATE,ADDRESS,STATUS,faculty.FACULTY_ID,faculty.FACULTY_NAME \
+                        FROM student,faculty WHERE student.FACULTY_ID=faculty.FACULTY_ID and ID = %s", id)
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        cnx.close()
+
+@app.route('/subject')
+def subject():
+    try:
+        cnx = mysql.connect()
+        cursor = cnx.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM subject")
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        cnx.close()
+
+@app.route('/subject/<id>')
+def subject_id(id):
+    try:
+        cnx = mysql.connect()
+        cursor = cnx.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM subject WHERE SUBJECT_ID = %s", id)
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        cnx.close()
+
+@app.route('/student/<id>/grades') # check
+def student_grade(id):
+    try:
+        cnx = mysql.connect()
+        cursor = cnx.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT ID,subject.SUBJECT_ID,subject.SUBJECT_NAME,GRADE \
+                        FROM course_enroll,subject \
+                        WHERE course_enroll.SUBJECT_ID=subject.SUBJECT_ID and ID = %s", id)
+        rows = cursor.fetchall()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        cnx.close()
+
+@app.route('/faculty')
+def faculty():
+    cnx = mysql.connect()
+    cursor = cnx.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT * FROM faculty ")
+    rows = cursor.fetchall()
+    resp = jsonify(rows)
+    resp.status_code = 200
+    cursor.close()
+    cnx.close()
+    return resp
